f69f85e42115abeb91834cb13c404d250a898c3b
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-12-16 15:06:55 +0900

    Initial Commit

---
+++ PracticeAnimation/PracticeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeAnimationApp.swift
+//  PracticeAnimation
+//
+//  Created by 이안진 on 12/16/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
